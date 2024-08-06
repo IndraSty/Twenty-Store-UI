f2d4e854c642bb2d55ff7363154914009717c870
Indoradesu <indrastyawan0925@gmail.com>
2024-08-06 22:17:42 +0700

    update home splash screen page view

--- lib/src/splash_screen/views/splashscreen_page.dart
+++ lib/src/splash_screen/views/splashscreen_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:twenty_store/common/services/storage.dart';
+import 'package:twenty_store/const/resource.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
 
   @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    _navigator();
+    super.initState();
+  }
+
+  _navigator() async {
+    await Future.delayed(
+      const Duration(microseconds: 3000),
+      () {
+        if (Storage().getBool('firstOpen') == null) {
+          GoRouter.of(context).go('/onboarding');
+        } else {
+          GoRouter.of(context).go('/home');
+        }
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              R.ASSETS_IMAGES_SPLASHSCREEN_PNG,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
