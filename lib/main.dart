8346cae309eb9cb82bf0988a6290e2c1aa6496f6
Indoradesu <indrastyawan0925@gmail.com>
2024-08-06 22:14:07 +0700

    update main setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:provider/provider.dart';
+import 'package:twenty_store/common/utils/app_routes.dart';
+import 'package:twenty_store/common/utils/environment.dart';
+import 'package:twenty_store/common/utils/kstrings.dart';
+import 'package:twenty_store/src/categories/controllers/category_notifier.dart';
+import 'package:twenty_store/src/entrypoint/controllers/bottom_tab_notifier.dart';
+import 'package:twenty_store/src/home/controllers/home_tabs_notifier.dart';
+import 'package:twenty_store/src/onboarding/controller/onboarding_controller.dart';
+import 'package:twenty_store/src/splash_screen/views/splashscreen_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
-  runApp(const MyApp());
+  await dotenv.load(fileName: Environment.fileName);
+  GetStorage.init();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => OnboardingNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => TabIndexNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CategoryNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => HomeTabsNotifier(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,61 +42,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    Size screenSize = MediaQuery.of(context).size;
+    return ScreenUtilInit(
+      designSize: screenSize,
+      minTextAdapt: true,
+      splitScreenMode: false,
+      useInheritedMediaQuery: true,
+      builder: (_, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: AppText.kAppName,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: router,
+        );
+      },
+      child: const SplashScreenPage(),
     );
   }
 }
