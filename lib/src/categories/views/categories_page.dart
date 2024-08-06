6a329b125c999ed21abd32c823121fee76d95f56
Indoradesu <indrastyawan0925@gmail.com>
2024-08-06 22:14:37 +0700

    update categories page view

--- lib/src/categories/views/categories_page.dart
+++ lib/src/categories/views/categories_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:twenty_store/common/utils/kcolors.dart';
 import 'package:twenty_store/common/utils/kstrings.dart';
 import 'package:twenty_store/common/widgets/app_style.dart';
 import 'package:twenty_store/common/widgets/back_button.dart';
 import 'package:twenty_store/common/widgets/reusable_text.dart';
+import 'package:twenty_store/const/constants.dart';
+import 'package:twenty_store/src/categories/controllers/category_notifier.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
@@ -17,6 +24,38 @@ class CategoriesPage extends StatelessWidget {
           text: AppText.kCategories,
           style: appStyle(16, Kolors.kPrimary, FontWeight.bold),
         ),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          final category = categories[index];
+          return ListTile(
+            onTap: () {
+              context.read<CategoryNotifier>().setCategory(
+                    category.title,
+                    category.id,
+                  );
+              context.push('/category');
+            },
+            leading: CircleAvatar(
+              backgroundColor: Kolors.kSecondaryLight,
+              radius: 18,
+              child: Padding(
+                padding: EdgeInsets.all(8.h),
+                child: SvgPicture.network(category.imageUrl),
+              ),
+            ),
+            title: ReusableText(
+              text: category.title,
+              style: appStyle(12, Kolors.kGray, FontWeight.normal),
+            ),
+            trailing: const Icon(
+              MaterialCommunityIcons.chevron_double_right,
+              size: 18,
+            ),
+          );
+        },
       ),
     );
   }
