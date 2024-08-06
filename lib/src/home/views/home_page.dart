e5c44205a662211de75d2b0f3e1c30d0e0a05891
Indoradesu <indrastyawan0925@gmail.com>
2024-08-06 22:16:06 +0700

    update home page view

--- lib/src/home/views/home_page.dart
+++ lib/src/home/views/home_page.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:twenty_store/src/home/controllers/home_tabs_notifier.dart';
+import 'package:twenty_store/src/home/widgets/categories_list.dart';
 import 'package:twenty_store/src/home/widgets/custom_app_bar.dart';
 import 'package:twenty_store/src/home/widgets/home_header.dart';
 import 'package:twenty_store/src/home/widgets/home_slide.dart';
+import 'package:twenty_store/src/home/widgets/home_tabs.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+  int _currentTabIndex = 0;
+
+  @override
+  void initState() {
+    _tabController = TabController(
+      length: homeTabs.length,
+      vsync: this,
+    );
+    _tabController.addListener(_handleSelection);
+    super.initState();
+  }
+
+  void _handleSelection() {
+    final controller = Provider.of<HomeTabsNotifier>(context, listen: false);
+    if (_tabController.indexIsChanging) {
+      setState(() {
+        _currentTabIndex = _tabController.index;
+      });
+      controller.setIndex(homeTabs[_currentTabIndex]);
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleSelection);
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +64,22 @@ class HomePage extends StatelessWidget {
             height: 15.h,
           ),
           const HomeHeader(),
+          SizedBox(
+            height: 15.h,
+          ),
+          const HomeCategoriesList(),
+          HomeTabs(tabController: _tabController),
         ],
       ),
     );
   }
 }
+
+List<String> homeTabs = [
+  "All",
+  "Popular",
+  "Unisex",
+  'Men',
+  'Women',
+  'Kid',
+];
